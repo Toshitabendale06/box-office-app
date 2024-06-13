2b59bda7f76c3e801932b453b5607d14eb6f608f
Toshita benale <toshitabendale5@gmail.com>
2024-06-13 19:27:14 +0530

    installed react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/Starred">Go to starred page</Link>
+    </div>
+  );
+};
+export default Home;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,4 @@
+const Starred = () => {
+  return <div>Starred pages</div>;
+};
+export default Starred;
