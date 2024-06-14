fc04b4749b8f9383a57fc7b5b2ad800d35dd0751
Toshita benale <toshitabendale5@gmail.com>
2024-06-14 18:40:22 +0530

    added search for actors

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -3,5 +3,8 @@ const apiGet = async queryString => {
   const response = await fetch(`${BASE_URL}${queryString}`);
   const body = await response.json();
   return body;
+  
 };
 export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
+
+export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
